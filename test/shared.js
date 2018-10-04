7a1ec347049b841bba1931566de073407e0c117c
emi <emiliorizzo@gmail.com>
2018-10-04 06:02:46 -0300

    Update tests

--- test/shared.js
+++ test/shared.js
@@ -1,77 +1,84 @@
 import { expect } from 'chai'
-import { Nod3 } from '../src/'
 import { isBlockHash } from '../src/utils'
 
-export const testNod3 = options => {
-  let provider = options.provider
+export const testNod3 = (nod3, options) => {
   let blockNumber = options.blockNumber
-  let nod3 = new Nod3(provider)
 
-  describe('isConnected', () => {
-    it('should be return a boolean', async () => {
-      let connected = await nod3.isConnected()
-      expect(connected).to.be.equal(true || false)
-      describe('getBlock', () => {
-        let block
-        it('should be return a block by decimal number', async () => {
-          block = await nod3.getBlock(blockNumber)
-          expect(block.number).to.be.equal(parseInt(blockNumber))
-        })
-        it('should be return a block by hash', async () => {
-          let hash = block.parentHash
-          block = null
-          block = await nod3.getBlock(hash)
-          expect(block.number).to.be.equal(blockNumber - 1)
-          expect(block.hash).to.be.equal(hash)
-        })
-      })
+  describe('getBlock', () => {
+    let block
 
-      describe('getBlocksByNumber', () => {
-        it('should be return an array', async () => {
-          let blocks = await nod3.getBlocksByNumber(blockNumber)
-          expect(blocks).to.be.an('array')
+    it('should be return a block by decimal number', async () => {
+      block = await nod3.eth.getBlock(blockNumber)
+      expect(block.number).to.be.equal(parseInt(blockNumber))
+    })
 
-        })
-      })
+    it('should be return a block by hash', async () => {
+      let hash = block.parentHash
+      block = null
+      block = await nod3.eth.getBlock(hash)
+      expect(block.number).to.be.equal(blockNumber - 1)
+      expect(block.hash).to.be.equal(hash)
+    })
+  })
 
-      let params = [...new Array(20)].map(a => [Math.floor(Math.random() * 600000), false])
-      let total = params.length
+  describe('getBlocksByNumber', () => {
 
-      describe(`batchRequest getBlock [${total}] by NUMBER `, () => {
+    it('should be return an array', async () => {
+      let blocks = await nod3.rsk.getBlocksByNumber(blockNumber)
+      expect(blocks).to.be.an('array')
+    })
+  })
 
-        it(`should be return an array of ${total} blocks`, async () => {
-          let data = await nod3.batchRequest('getBlock', params)
-          expect(data).to.be.an('array')
-          expect(data.length === total)
+  let params = [...new Array(10)].map(a => [Math.floor(Math.random() * 600000), false])
+  let total = params.length
+
+  describe(`batch request: getBlock [${total}]`, () => {
+    describe(`# by NUMBER `, () => {
+    
+      it(`should be return an array of ${total} values`, async () => {
+        let data = await nod3.batchRequest(params, 'eth.getBlock')
+        expect(data).to.be.an('array')
+        expect(data.length).to.be.equal(total)
+
+        describe('Test values', () => {
           data.forEach(block => {
             testBlock(block)
           })
-          params = new Set()
-          data.forEach(block => {
-            params.add(block.parentHash)
-            return block.uncles.forEach(u => params.add(u))
-          })
-          params = [...params].slice(0, total).map(h => [h, false])
+        })
 
-          describe(`batchRequest getBlock [${total}] by HASH `, () => {
-            it(`should be return an array of ${total} blocks`, async () => {
-              let data = await nod3.batchRequest('getBlock', params)
-              expect(data).to.be.an('array')
-              expect(data.length === total)
-              data.forEach(block => {
-                testBlock(block)
-              })
+        params = new Set()
+        data.forEach(block => {
+          params.add(block.parentHash)
+          return block.uncles.forEach(u => params.add(u))
+        })
+        params = [...params].slice(0, total).map(h => [h, false])
+
+        describe(`# by HASH `, () => {
+          it(`should be return an array of ${total} blocks`, async () => {
+            let data = await nod3.batchRequest(params, 'eth.getBlock')
+            expect(data).to.be.an('array')
+            expect(data.length).to.be.equal(total)
+            data.forEach(block => {
+              testBlock(block)
             })
           })
         })
       })
     })
   })
+
+  
+
+  describe(`Mixed methods Batch request`, () => {
+
+  })
+
 }
 
 export const testBlock = block => {
-  describe(`block ${(block) ? block.number : block}`, () => {
-    it(`should be a block`, () => {
+  describe(`Testing block ${(block) ? block.number : block}`, () => {
+
+    it(`should have block properties`, () => {
       expect(block.number).to.be.an('number')
       expect(isBlockHash(block.hash)).to.be.equal(true)
       expect(isBlockHash(block.parentHash)).to.be.equal(true)
--- test/test.js
+++ test/test.js
@@ -1,10 +1,24 @@
-import { HttpProvider } from '../src/'
+import { Nod3, HttpProvider } from '../src/'
 import { testNod3 } from './shared'
+import { expect } from 'chai'
+
 const blockNumber = process.env['blockNumber'] || Math.floor(Math.random() * 600000)
+global.blockNumber = blockNumber
 
 describe(`Testing with blockNumber:${blockNumber}`, () => {
   describe(`HTPP provider`, () => {
-    let provider = new HttpProvider()
-    testNod3({ blockNumber, provider })
+    let nod3 = new Nod3(new HttpProvider())
+    describe('isConnected', () => {
+      it('should be return a boolean', async () => {
+        let connected = await nod3.isConnected()
+        expect(connected).to.be.equal(true || false)
+        describe('Nod3 test', () => {
+          it('should be connected', () => {
+            expect(connected).to.be.equal(true)
+            testNod3(nod3, { blockNumber })
+          })
+        })
+      })
+    })
   })
 })
