ec9c394637913fe816cbfd2b2f121863319a4606
emi <emiliorizzo@gmail.com>
2018-10-14 04:43:59 +0000

    Update filters, add pendingTransaction

--- src/lib/filters.js
+++ src/lib/filters.js
@@ -2,14 +2,22 @@ export default {
   newBlock () {
     return {
       method: 'eth_newBlockFilter',
-      cb (err, data) {
-        // return error to original handler
-        if (err) return this.emit(err)
-        // emit data if not empty
-        if (data && data.length) {
-          data.forEach(d => this.emit(null, d))
-        }
-      }
+      cb: defaultCb
     }
+  },
+  pendingTransactions () {
+    return {
+      method: 'eth_newPendingTransactionFilter',
+      cb: defaultCb
+    }
+  }
+}
+
+function defaultCb (err, data) {
+  // return error to original handler
+  if (err) return this.emit(err)
+  // emit data if not empty
+  if (data && data.length) {
+    data.forEach(d => this.emit(null, d))
   }
 }
