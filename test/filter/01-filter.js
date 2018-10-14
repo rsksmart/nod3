864c2512fd6c0dbe46b76a609af950812bb6ef77
emi <emiliorizzo@gmail.com>
2018-10-14 04:44:11 +0000

    Update tests

---
+++ test/filter/01-filter.js
@@ -0,0 +1,86 @@
+import { nod3Creator } from '../shared'
+import { Subscription } from '../../src/classes/Subscription'
+import { expect } from 'chai'
+import { isBlockHash } from '../../src/lib/utils'
+
+const nod3 = nod3Creator()
+var nbFilter
+let filtersTests = [
+  {
+    name: 'newBlock',
+    validator: function (data) {
+      return isBlockHash(data)
+    }
+  },
+  {
+    name: 'pendingTransactions',
+    validator: function (data) {
+      // UNCOMPLETE
+      return Array.isArray(data)
+    },
+    skip: true
+  }
+]
+// remove skipped tests
+filtersTests = filtersTests.filter(f => !f.skip)
+
+filtersTests.forEach(f => {
+  var filterName = f.name
+  var validator = f.validator
+  if (!filterName || !validator) throw new Error('Bad configuration')
+  describe(`add ${filterName} filter`, () => {
+
+    it(`should create a ${filterName} filter`, async function () {
+      nbFilter = await nod3.subscribe.filter(filterName)
+      expect(nbFilter).to.be.instanceOf(Subscription)
+    })
+  })
+
+  describe(`waiting for ${filterName} results`, function () {
+    it('sould be a valid result', function (done) {
+      this.timeout(60000)
+      nbFilter.on('data', data => {
+        console.log(data)
+        expect(data).to.satisfy(validator)
+        done()
+      })
+    })
+  })
+
+  describe(`remove ${filterName} filter`, function () {
+    it('should remove filter', async function () {
+      let id = nbFilter.id
+      await nbFilter.delete()
+      expect(nod3.subscribe.subscriptions.get(id)).to.be.equal(undefined)
+      expect(nod3.provider.pool.get(id)).to.be.equal(undefined)
+      let nodeFilter = await nod3.rpc.sendMethod('eth_getFilterChanges', id)
+      expect(nodeFilter).to.be.equal(null)
+      return Promise.resolve()
+    })
+  })
+})
+
+
+let sub
+
+describe('subscribe to method eth.syncing', () => {
+  it('should create a subscription', async function () {
+    sub = await nod3.subscribe.method('eth.syncing')
+    expect(sub).to.be.instanceOf(Subscription)
+    return Promise.resolve()
+  })
+})
+
+describe('waiting subscription events', function () {
+  it('sould be a valid method event', function (done) {
+    this.timeout(60000)
+    sub.on('data', data => {
+      console.log(data)
+      expect(data).to.satisfy(function (data) {
+        return data === true || data === false || typeof data === 'object'
+      })
+      sub.delete()
+      done()
+    })
+  })
+})
---
+++ test/filter/02-subscribe.clear.js
@@ -0,0 +1,45 @@
+import { nod3Creator } from '../shared'
+import { expect } from 'chai'
+const nod3 = nod3Creator()
+
+describe('Test subscribe.clear', function () {
+  let list = [
+    'newBlock',
+    'eth.syncing',
+    ['rsk.getBlocksByNumber', '0x05'],
+    'newBlock',
+    ['eth.getBlock', '0xf', false]
+  ]
+
+  it(`should create ${list.length} subscriptions`, async function () {
+    let connected = await nod3.isConnected()
+    if (!connected) throw new Error('nod3 is not connected')
+    await createSubs(list)
+    let subs = nod3.subscribe.list()
+    expect(subs.length).to.be.equal(list.length)
+  })
+
+  it('should be remove all subscriptions', async function () {
+    await nod3.subscribe.clear()
+    let total = nod3.subscribe.list()
+    expect(total).to.be.an('array')
+    expect(total.length).to.be.equal(0)
+  })
+})
+
+async function createSubs (subs) {
+  try {
+    let q = []
+    subs.forEach(async s => {
+      let n = (Array.isArray(s)) ? s[0] : s
+      let type = (n.split('.').length > 1) ? 'method' : 'filter'
+      let params = (Array.isArray(s)) ? s.slice(1) : []
+      q.push(nod3.subscribe[type](n, params))
+    })
+    let res = await Promise.all(q)
+    return res
+  } catch (err) {
+    console.log(err)
+    process.exit()
+  }
+}
