cd0ea1d5c4c3d58c8705e1e882baba566a8c08e0
emi <emiliorizzo@gmail.com>
2018-11-03 01:05:14 +0000

    Update txpool module, fix rskj txpool bad responses
    See: https://github.com/rsksmart/rskj/issues/689

--- src/modules/txpool.js
+++ src/modules/txpool.js
@@ -13,4 +13,6 @@ export default {
   }
 }
 
-export const txPoolFormatter = result => JSON.parse(result)
+// fix bad rskj response
+// see: https://github.com/rsksmart/rskj/issues/689
+export const txPoolFormatter = result => (typeof result === 'string') ? JSON.parse(result) : result
--- test/txpool/01-txPool.js
+++ test/txpool/01-txPool.js
@@ -4,7 +4,7 @@ import { nod3Creator } from '../shared'
 let nod3 = global.nod3 || nod3Creator()
 
 describe('# txpool content', function () {
-  it('should....', async function () {
+  it('should be a valid response', async function () {
     let content = await nod3.txpool.content()
     expect(content).has.ownProperty('pending')
     expect(content).has.ownProperty('queued')
@@ -12,7 +12,7 @@ describe('# txpool content', function () {
 })
 
 describe('# txpool inspect', function () {
-  it('should....', async function () {
+  it('should be a valid response', async function () {
     let inspect = await nod3.txpool.inspect()
     expect(inspect).has.ownProperty('queued')
     expect(inspect).has.ownProperty('pending')
@@ -20,7 +20,7 @@ describe('# txpool inspect', function () {
 })
 
 describe('# txpool status', function () {
-  it('should....', async function () {
+  it('should be a valid response', async function () {
     let status = await nod3.txpool.status()
     expect(status.queued).to.be.a('number')
     expect(status.pending).to.be.a('number')
