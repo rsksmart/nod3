afe2a1884fbd371c3368c7c749e99f06beaf2f28
emi <emiliorizzo@gmail.com>
2019-04-23 04:22:28 +0000

    Refactor examples, add generator

--- test/examples/index.js
+++ test/examples/index.js
@@ -1,14 +1,27 @@
 import path from 'path'
 import fs from 'fs'
-const p = path.resolve(__dirname)
+const basePath = path.resolve(__dirname)
 
-export const getExample = (methodName, params) => {
+export const getExamples = (methodName, params) => {
   try {
     const [module, method] = methodName.split('_')
-    let example = fs.readFileSync(`${p}/${module}/${method}/01.json`).toString()
-    example = JSON.parse(example)
-    let { result } = example
-    return result
+    if (!module || !method) throw new Error(`invalid methodName ${methodName}`)
+    const examplePath = `${basePath}/${module}/${method}`
+    const examples = fs.readdirSync(`${examplePath}`)
+    const total = (Array.isArray(examples)) ? examples.length : undefined
+    if (!total) throw new Error(`the example folder: ${examplePath} is empty`)
+    let key = 0
+    const next = () => {
+      if (!examples[key]) return
+      const file = `${examplePath}/${examples[key]}`
+      key++
+      let example = fs.readFileSync(file).toString()
+      example = JSON.parse(example)
+      let { result } = example
+      return result
+    }
+
+    return Object.freeze({ next, total })
   } catch (err) {
     console.error(err)
     process.exit(9)
