8442c0699d05a86158aff88a0c986647250dfdf9
emi <emiliorizzo@gmail.com>
2019-10-09 03:49:03 +0000

    Update eth module, add call method

--- src/modules/eth.js
+++ src/modules/eth.js
@@ -5,12 +5,12 @@ import {
   txReceiptFormatter
 } from '../lib/formatters'
 
-import { isHashOrNuber, toHexStr, toDecimal } from '../lib/utils'
+import { isHashOrNumber, toHexStr, toDecimal } from '../lib/utils'
 
 export default {
   getBlock (hashOrNumber, txs = false) {
     let method = 'eth_getBlockByNumber'
-    let hOn = isHashOrNuber(hashOrNumber)
+    let hOn = isHashOrNumber(hashOrNumber)
 
     if (hOn.hash) method = 'eth_getBlockByHash'
     else hashOrNumber = (hOn.number !== null) ? hOn.number : hashOrNumber
@@ -20,7 +20,7 @@ export default {
   },
 
   getTransactionByIndex (hashOrNumber, index) {
-    let hOn = isHashOrNuber(hashOrNumber)
+    let hOn = isHashOrNumber(hashOrNumber)
     let params = (hOn.hash) ? [hOn.hash] : [hOn.number]
     let method = (hOn.hash) ? 'eth_getTransactionByBlockHashAndIndex' : 'eth_getTransactionByBlockNumberAndIndex'
     index = toHexStr(index)
@@ -66,14 +66,20 @@ export default {
   sendRawTransaction (txData) {
     return { method: 'eth_sendRawTransaction', params: [txData] }
   },
+
   netHashrate () {
     return { method: 'eth_netHashrate', formatters: [toDecimal] }
+  },
+
+  call (callObj, block = 'latest') {
+    block = parseBlockArg(block)
+    return { method: 'eth_call', params: [callObj, block] }
   }
 
 }
 
 function parseBlockArg (block) {
-  let hOn = isHashOrNuber(block)
+  let hOn = isHashOrNumber(block)
   if (hOn.hash) block = hOn.hash
   if (hOn.number) block = hOn.number
   return block
