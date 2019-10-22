0638369f671bd220b7d9d5d8888673b1260372dd
emi <emiliorizzo@gmail.com>
2019-10-22 15:09:18 +0000

    Build 0.0.13

--- dist/modules/trace.js
+++ dist/modules/trace.js
@@ -1,4 +1,7 @@
 "use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _default = {
   transaction(txHash) {
     return { method: 'trace_transaction', params: [txHash] };
+  },
+  block(blockHash) {
+    return { method: 'trace_block', params: [blockHash] };
   } };exports.default = _default;
