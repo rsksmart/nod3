6496f0637ef086801242edbb35245a6e894792c9
emi <emiliorizzo@gmail.com>
2019-11-06 05:01:44 +0000

    Refactor JsonRpc

--- src/classes/JsonRpc.js
+++ src/classes/JsonRpc.js
@@ -64,15 +64,15 @@ export class JsonRpc {
   }
 
   isValidResponse (res) {
-    const isValidId = this.isValidId
-    return Array.isArray(res) ? res.every(validate) : validate(res)
-    function validate (message) {
+    const validate = message => {
       return !!message &&
         !message.error &&
         message.jsonrpc === '2.0' &&
         isValidId(message.id) &&
         message.result !== undefined
     }
+    const isValidId = this.isValidId
+    return Array.isArray(res) ? res.every(validate) : validate(res)
   }
 }
 
