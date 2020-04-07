71d222c2f7735c4547b7f7db648cbcbd5df01f84
emi <emiliorizzo@gmail.com>
2020-04-07 10:06:12 -0600

    Update test/trace

--- test/trace/trace.spec.js
+++ test/trace/trace.spec.js
@@ -2,13 +2,13 @@ import { expect } from 'chai'
 import { nod3Creator, checkDecimal, getRandomBlockNumber } from '../shared'
 let nod3 = global.nod3 || nod3Creator()
 const txs = ['0x3dc63d1963891243522bb5cca95127afbefa8b2a9dde6e62e5cd95c35fda8457']
-const blocks = [...Array(2)].map(() => getRandomBlockNumber())
+const blocks = [...Array(1)].map(() => getRandomBlockNumber())
 describe(`# trace`, function () {
   describe(`trace_transaction()`, function () {
     it(`should return a transaction trace`, async () => {
       for (let tx of txs) {
         const trace = await nod3.trace.transaction(tx)
-        expect(trace).to.be.an('array')
+        expect(trace).to.be.an('array', 'trace should be an array')
         for (let action of trace) {
           testAction(action)
         }
@@ -24,15 +24,13 @@ describe(`# trace`, function () {
         const block = await nod3.eth.getBlock(hash, false)
         const { transactions } = block
         const trace = await nod3.trace.block(hash)
-        expect(trace).to.be.an('array')
-        expect(trace.length).to.be.equal(transactions.length)
-        const hashes = trace.map(item => [...new Set(item.map(a => a.transactionHash))]).map(a => a.pop())
-        expect(hashes).to.be.deep.equal(transactions)
+        expect(trace).to.be.an('array', 'trace should be an array')
+        const hashes = new Set(...[trace.map(t => t.transactionHash)])
+        expect(hashes.size).to.be.equal(transactions.length, 'transactions length')
+        expect([...hashes]).to.be.deep.equal(transactions, 'transactions hashes')
         for (let item of trace) {
-          expect(item).to.be.an('array')
-          for (let action of item) {
-            testAction(action)
-          }
+          expect(item).to.be.an('object')
+          testAction(item)
         }
       })
     }
@@ -40,7 +38,7 @@ describe(`# trace`, function () {
 })
 
 function testAction (action) {
-  expect(action).to.be.an('object')
-  expect(action).includes.all.keys(['blockNumber', 'blockHash', 'transactionHash', 'action', 'result'])
+  expect(action).to.be.an('object', 'action should be an object')
+  expect(Object.keys(action)).to.include.members(['blockNumber', 'blockHash', 'transactionHash', 'action', 'result'], 'action properties')
   checkDecimal(action.blockNumber)
 }
