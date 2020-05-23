3705c0da782e50a60d825d9819a7b83a073dfb47
emi <emiliorizzo@gmail.com>
2020-05-23 17:38:31 -0600

    Test, refactor Nod3Hub test

--- test/Nod3Hub.spec.js
+++ test/Nod3Hub.spec.js
@@ -3,34 +3,33 @@ import { HttpProvider } from '../src/classes/HttpProvider'
 import { assert } from 'chai'
 import { JsonRpcServer } from './Servers/JsonRpcServer'
 
-const urls = ['http://127.0.0.1:9000', 'http://bar.baz']
+const host = 'http://127.0.0.1'
+const urls = [7007, 8008, 9009].map(port => `${host}:${port}`)
 
-let block = 0
-const server = JsonRpcServer(urls[0])
-server.addMethod('eth_blockNumber', () => block++)
-server.addMethod('eth_newBlockFilter', () => 1)
+const servers = urls.map(url => createServer(url))
 
 describe(`# Nod3Hub`, function () {
-  this.afterAll(() => server.close())
+  this.afterAll(() => servers.map(server => server.close()))
   const nod3 = Nod3Hub(urls.map(url => new HttpProvider(url)))
+
   it('should be a nod3Hub instance', () => {
     assert.equal(nod3[NOD3_HUB_NAME], true)
   })
 
-  it(`should return the first instance`, () => {
-    assert.equal(nod3.provider.url, urls[0])
+  it(`should return a different instance every time`, () => {
+    for (let url of urls.concat(urls)) {
+      assert.equal(nod3.provider.url, url)
+    }
   })
 
-  it(`should return the next instance`, () => {
-    assert.equal(nod3.provider.url, urls[1])
-  })
-
-  it(`should return the first instance`, () => {
-    assert.equal(nod3.provider.url, urls[0])
-  })
-
-  it(`should return the next instance`, () => {
-    assert.equal(nod3.provider.url, urls[1])
+  it(`debugging should be set on all instances`, async () => {
+    let log = []
+    nod3.setDebug(obj => log.push(obj))
+    const requests = urls.concat(urls)
+    await Promise.all(requests.map(() => nod3.eth.blockNumber()))
+    assert.equal(log.length, requests.length)
+    let nodes = log.map(({ url }) => url).filter((v, i, s) => s.indexOf(v) === i)
+    assert.includeMembers(nodes, urls)
   })
 
   it(`set operations should modify all instances`, () => {
@@ -58,4 +57,17 @@ function testSubscribe (nod3, type, name) {
     subscription.watch(() => subscription.delete())
     assert.equal(nod3.subscribe.provider.url, urls[0])
   })
+}
+
+function createServer (url) {
+  let block = 0
+  const server = JsonRpcServer(url)
+  server.addMethod('eth_blockNumber', () => {
+    return {
+      block: block++,
+      url
+    }
+  })
+  server.addMethod('eth_newBlockFilter', () => 1)
+  return server
 }
