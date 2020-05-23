08c690f52aeaa7a740452b77b47b7094bbe69a31
emi <emiliorizzo@gmail.com>
2020-05-23 01:50:35 -0600

    Update index, export Nod3Hub

--- releaseTest/index.js
+++ releaseTest/index.js
@@ -1,7 +1,15 @@
 const assert = require('assert')
-const { Nod3 } = require('nod3')
-const nod3 = new Nod3(new Nod3.providers.HttpProvider('http://localhost:4444'))
+const { Nod3, Nod3Hub, NOD3_HUB_NAME } = require('nod3')
+const { HttpProvider } = Nod3.providers
+
+const url = 'http://localhost'
+const nod3 = new Nod3(new HttpProvider(`${url}:4444`))
+const hub = Nod3Hub([`${url}:4444`, `${url}:4444`].map(u => new HttpProvider(u)))
 
 assert.equal(typeof nod3, 'object', 'nod3 is not an object')
 assert.equal(typeof nod3.eth.getBlock, 'function', 'eth.getBlock is not a function')
-console.log('done')
+
+assert.equal(typeof hub, 'object')
+assert.equal(hub[NOD3_HUB_NAME], NOD3_HUB_NAME)
+
+console.log('done!')
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { Nod3 as n3 } from './classes/Nod3'
+export { Nod3Hub } from './classes/Nod3Hub'
 export const Nod3 = n3
 
 export default Nod3
