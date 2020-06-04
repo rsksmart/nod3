6029114e46aef213c2d561c99e30eb9eebc8b797
emi <emiliorizzo@gmail.com>
2020-06-03 22:41:06 -0600

    Doc, rsk module

--- src/modules/rsk.js
+++ src/modules/rsk.js
@@ -1,4 +1,14 @@
+/**
+ * @module rsk
+ * @typicalname nod3.rsk
+ */
+
 export default {
+  /**
+   * Returns a list of block hashes for a given block number with the main chain block identified.
+   * @param {Number} blockNumber
+   * @returns {Promise.<Array>}: Array of block objects.
+   */
   getBlocksByNumber (number) {
     let method = 'eth_getBlocksByNumber'
     let params = [number]
