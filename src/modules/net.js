5ff09c1ae295664538a60585715001f2fce62c07
emi <emiliorizzo@gmail.com>
2020-06-03 22:31:00 -0600

    Doc, net module

--- src/modules/net.js
+++ src/modules/net.js
@@ -1,8 +1,27 @@
 import { netName, toDecimal } from '../lib/utils'
+
+/**
+ * @module net
+ * @typicalname nod3.net
+ */
+
 export default {
+  /**
+  *  Returns true if client is listening for networks connections.
+   * @returns {Promise.<Boolean>}
+   * @see https://github.com/ethereum/wiki/wiki/JSON-RPC#net_listening
+   */
+
   listening () {
     return { method: 'net_listening' }
   },
+
+  /**
+  * Returns the network id.
+  * @returns {Promise.<String>} network id
+  * @see https://github.com/ethereum/wiki/wiki/JSON-RPC#net_version
+  */
+
   version () {
     const formatVersion = (id) => {
       let name = netName(id)
@@ -10,6 +29,13 @@ export default {
     }
     return { method: 'net_version', formatters: [formatVersion] }
   },
+
+  /**
+  *  Returns number of peers connected to the client.
+  * @returns {Promise.<QUANTITY>} peers
+  * @see https://github.com/ethereum/wiki/wiki/JSON-RPC#net_peercount
+  */
+
   peerCount () {
     return { method: 'net_peerCount', formatters: [(value) => toDecimal(value)] }
   }
